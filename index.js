ecaa14bfa50af4106acf515661ced82605374815
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-21 09:57:19 +0700

    membuat index JS

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();  
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'biodata'
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});

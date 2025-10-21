98548da1e7008c818b15cefcd6996463687f5041
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-21 10:02:27 +0700

    fix app listen port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`app listening at http://localhost:${port}`);
 });
 
 const db = mysql.createConnection({
